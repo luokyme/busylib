0ac1a6447dd4b16882f3c4551229516232cb4384
Xavier <2488091@qq.com>
2025-09-15 20:58:44 +0800

    Fix logger stream bug and improve tests

--- test/logger_ut.cc
+++ test/logger_ut.cc
@@ -3,8 +3,13 @@
 
 using namespace busylib;
 
-TEST_CASE("vectors can be sized and resized", "[vector]")
+TEST_CASE("logger prints formatted messages", "[logger]")
 {
+  Logger::setLevel(Logger::LOG_INFO);
   logInfo("Test format string: 5 = %d", 5);
   logInfo("Test append string: 5 = ") << 5;
+
+  Logger::setLevel(Logger::LOG_DEBUG);
+  REQUIRE(Logger::level() == Logger::LOG_DEBUG);
+  Logger::setLevel(Logger::LOG_INFO);
 }
